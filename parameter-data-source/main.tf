173dc6a4845969e9dda643c64850b065c1571a94
praveenarupi <praveenarupi@gmail.com>
2022-10-20 21:32:16 +0530

    updating ./learning-terraform

---
+++ parameter-data-source/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ssm_parameter" "foo" {
+    name = "mutable.docdb.dev.credentials"
+}
+
+resource "local_file" "foo" {
+  content  = data.aws_ssm_parameter.foo.value
+  filename = "/tmp/out"
+}
